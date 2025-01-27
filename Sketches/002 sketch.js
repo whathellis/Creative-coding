4f2c2301ae6545f10645e6345f8a96fa5efdeadc
mashroomash <cocomoc95@gmail.com>
2025-01-27 13:48:09 +1000

    Add files via upload

--- Sketches/002 sketch.js
+++ Sketches/002 sketch.js
@@ -9,38 +9,25 @@ const colours = [
   "#1b82e6",
   "#6d5acf",
 ];
-let b=0;
-const d = [
-  10,
-  11,
-  12,
-  13,
-  14,
-  15,
-  16,
-  17,
-  18,
-  19,
-  20,
-];
+let b = 0;
+const d = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(45,45,45);
+  background(45, 45, 45);
 }
 
 function draw() {
-  
-  currentColour = color(45,45,45);
+  currentColour = color(45, 45, 45);
   targetColour = color(70, 0, 200);
 
   lerpColor(currentColour, targetColour, 0.15);
 
   circle(mouseX, mouseY, b);
-  if (mouseIsPressed){b = random(d)*random(d)/5;fill(random(colours));}
-  else { b= 20;fill(currentColour);}
-
- 
- 
-  
-
+  if (mouseIsPressed) {
+    b = (random(d) * random(d)) / 5;
+    fill(random(colours));
+  } else {
+    b = 20;
+    fill(currentColour);
+  }
 }
---
+++ Sketches/006.js
@@ -0,0 +1,8 @@
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background(50,34,72);
+  circle(mouseX, mouseY, 20);
+}
